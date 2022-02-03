32f4c00125a186c18a7d86b77d69d9e558f06d23
Raymond Zhao <raymondzhao@microsoft.com>
2022-02-03 15:13:44 -0800

    Adjust min widths

--- src/vs/workbench/contrib/preferences/browser/settingsEditor2.ts
+++ src/vs/workbench/contrib/preferences/browser/settingsEditor2.ts
@@ -94,7 +94,7 @@ export class SettingsEditor2 extends EditorPane {
 	private static CONFIG_SCHEMA_UPDATE_DELAYER = 500;
 	private static TOC_MIN_WIDTH: number = 100;
 	private static TOC_RESET_WIDTH: number = 200;
-	private static EDITOR_MIN_WIDTH: number = 600;
+	private static EDITOR_MIN_WIDTH: number = 500;
 	private static NARROW_TOTAL_WIDTH: number = SettingsEditor2.TOC_MIN_WIDTH + SettingsEditor2.EDITOR_MIN_WIDTH;
 	private static MEDIUM_TOTAL_WIDTH: number = 1000;
 
